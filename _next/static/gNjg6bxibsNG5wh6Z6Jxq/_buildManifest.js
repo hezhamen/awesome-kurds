144f737ff078fca4ac16d21d2b4d43276e6b74ad
AramRafeq <aramrafeq2@gmail.com>
2021-12-25 21:29:51 +0000

    Deploying to gh-pages from @ AramRafeq/awesome-kurds@c7fdf3e869f734d9c9cd8dc5a05bad2d68e9d0d2 🚀

---
+++ _next/static/gNjg6bxibsNG5wh6Z6Jxq/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/955-aa29982fabb30705.js","static/css/ad47a3cdab8a3ae2.css","static/chunks/pages/index-1a480713d28185d0.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
