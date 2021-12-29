75488773800245f549226c6c736fdffdb4f66540
Roj <ez@roj.im>
2021-12-29 13:30:49 +0300

    Readd links in intro

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -4,12 +4,15 @@ export default function Intro({ kurds }: { kurds: KurdsWithTopics }) {
   return (
     <div className="intro">
       <div>
-        <h1>
-          <a href="https://github.com/DevelopersTree/awesome-kurds">
-            Awesome Kurds
-          </a>
-        </h1>
+        <h1>Awesome Kurds</h1>
         <h3>Meet {kurds.length}+ Awesome Kurds.</h3>
+        <p>
+          <a href="https://github.com/DevelopersTree/awesome-kurds">
+            Add Yourself
+          </a>{" "}
+          &middot;{" "}
+          <a href="https://github.com/hezhamen/awesome-kurds">Contribute</a>
+        </p>
       </div>
       <div className="scroll-down"></div>
     </div>
