f3030b657413805b04b15653add045dba2bdf855
Roj <ez@roj.im>
2021-12-29 12:28:42 +0300

    Add tag filter

--- components/Cards.tsx
+++ components/Cards.tsx
@@ -7,30 +7,35 @@ export default function Cards({
 }: {
   awesomeKurds: AwesomeKurds;
 }) {
+  const [tagQuery, setTagQuery] = useState("all");
   const [searchQuery, setSearchQuery] = useState("");
 
   return (
     <>
       <div className="search">
+        <select onChange={e => setTagQuery(e.target.value)}>
+          <option key="all" value="all" selected>
+            All
+          </option>
+          {awesomeKurds.tags.map((t, i) => (
+            <option key={i} value={t}>
+              {t}
+            </option>
+          ))}
+        </select>
         <input
           type="text"
           placeholder="Search by name..."
-          // @ts-ignore
-          onKeyUp={e => setSearchQuery(e.target.value)}
+          onKeyUp={e => setSearchQuery((e.target as HTMLInputElement).value)}
         />
-        {/* <select>
-        <option key="all" selected>All</option>
-        {awesomeKurds.tags.map((t, i) => (
-          <option key={i} value={t}>
-            {t}
-          </option>
-        ))}
-      </select> */}
       </div>
       <div className="cards">
-        {awesomeKurds.searchForKurd(searchQuery).map((k, i) => (
-          <Card key={i} kurd={k} />
-        ))}
+        {awesomeKurds
+          .searchForKurd(searchQuery)
+          .filter(k => tagQuery == "all" || k.tags.includes(tagQuery))
+          .map((k, i) => (
+            <Card key={i} kurd={k} />
+          ))}
       </div>
     </>
   );
